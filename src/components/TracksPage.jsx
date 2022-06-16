9a8d66e0b09e5b7824f30ed4b609c48c7b1a8983
Lyn <lynfinman@gmail.com>
2022-06-16 15:18:44 -0400

    filtered track title

--- src/components/TracksPage.jsx
+++ src/components/TracksPage.jsx
@@ -6,12 +6,17 @@ import TracksList from './TracksList'
 function TracksPage() {
 const [tracks, setTracks] = useState([]);
 const [searchTerm, setSearchTerm] = useState("");
+// filteredTracks = [];
 
 useEffect(()=> {
   fetch("http://localhost:8001/tracks")
   .then(r=>r.json())
   .then(tracks=>setTracks(tracks))
 },[])
+
+// useEffect(() => {
+// filteredTracks = tracks.filter(track=>track.title.contains(searchTerm))
+// },[searchTerm, tracks])
 
 const updateSearchTerm = (newSearch) => {
   setSearchTerm(newSearch);
@@ -21,12 +26,14 @@ const handleAddTrack = (newTrack) => {
   setTracks([...tracks, newTrack]);
 }
     console.log('Search',searchTerm)
+    //console.log('Filtered tracks',filteredTracks)
+const filteredTracks = tracks.filter(track=>track.title.includes(searchTerm));
 
   return (
     <div>
-      <Search onUpdateSearch={updateSearchTerm}/>
+      <Search onUpdateSearch={updateSearchTerm} searchTerm={searchTerm}/>
       <AddTrackForm onAddTrack={handleAddTrack}/>
-      <TracksList tracks={tracks}/>
+      <TracksList tracks={filteredTracks}/>
     </div>
   )
 }
