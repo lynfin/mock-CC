8e7080f59d58fe8c776dac73f6ac42d70aed9658
Lyn <lynfinman@gmail.com>
2022-06-16 14:27:29 -0400

    displaying all tracks

--- src/components/TracksList.jsx
+++ src/components/TracksList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import Track from './Track'
 
-function TracksList() {
+function TracksList({tracks}) {
   return (
     <table>
       <tbody>
@@ -19,7 +20,7 @@ function TracksList() {
             <h3 className="">BPM</h3>
           </th>
         </tr>
-        {/* render a list of <Track> components here */}
+        {tracks.map(track=><Track track={track} key={track.id}/>)}
       </tbody>
     </table>
   )
--- src/components/TracksPage.jsx
+++ src/components/TracksPage.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Search from './Search'
 import AddTrackForm from './AddTrackForm'
 import TracksList from './TracksList'
 
 function TracksPage() {
+const [tracks, setTracks] = useState([]);
 
-    
+useEffect(()=> {
+  fetch("http://localhost:8001/tracks")
+  .then(r=>r.json())
+  .then(tracks=>setTracks(tracks))
+},[])
+    console.log(tracks)
   return (
     <div>
       <Search />
       <AddTrackForm />
-      <TracksList />
+      <TracksList tracks={tracks}/>
     </div>
   )
 }
